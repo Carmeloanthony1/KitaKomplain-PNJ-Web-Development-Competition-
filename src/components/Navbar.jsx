adc0903997ba0e5445c4ae26559f30985066f212
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-16 12:04:00 -0400

    Konfigurasi tailwind v4

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-between items-center py-4 px-8">
+      <h1 className="text-4xl font-extrabold text-[#9E1B32] tracking-tight">
+        KitaKomplain
+      </h1>
+    </div>
+  );
+}
