3d8d9665052b71a7c3776251ddc23372c2eda75f
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-17 00:16:12 -0400

    add: membuat sidebar serta menghubungkannya ke app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ export default function App(){
   return (
     <div className="bg-[#f7f7f7] min-h-screen p-4">
       <Navbar/>
+      <div className="flex flex-1 px-8 py-4 gap-6 w-full">
+        <aside className="w-1/5 flex-shrink-0">
+          <Sidebar_Kiri/>
+        </aside>
+      </div>
     </div>
   );  
 }
--- src/components/Sidebar_Kiri.jsx
+++ src/components/Sidebar_Kiri.jsx
@@ -3,7 +3,7 @@ export default function Sidebar_kiri(){
   const [search, setSearch] = useState("");
   
   return (
-    <aside className="w-64 flex flex-col justify-center h-[calc(100vh-100px)]">
+    <aside className="w-64 flex flex-col justify-between h-[calc(100vh-100px)]">
       <div className="flex flex-col gap-6">
         <button className="flex items-center gap-4 text-[#a50034]">
           <svg className = "w-7 h-7 fill-[#a50034]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
@@ -21,6 +21,14 @@ export default function Sidebar_kiri(){
           </svg> History
         </button>
       </div>
+
+      <div>
+        <button className="flex items-center gap-4 text-[#a50034]">
+          <svg className = "w-5 h-5 fill-[#951B32] flex-shrink-0 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+            <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/>
+          </svg> Setting
+        </button>
+      </div>  
     </aside>
   );  
 }
