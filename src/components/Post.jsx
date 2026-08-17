ba40f8bf36f342933e9c5e5d7e3f1bff497bc3b7
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-17 10:25:11 -0400

    add: remake Most Polling

---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Sidebar_kiri() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <aside className="w-64 flex flex-col justify-between py-0 px-2 pb-4 bg-[#f7f7f7]">
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-12">
+          <button className="flex items-center text-3xl gap-4 font-bold text-[#a50034] cursor-pointer hover:opacity-80 transition">
+            <svg className="w-12 h-12 fill-[#a50034] flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+              <path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/>
+            </svg> Post
+          </button>
+
+          <button className="flex items-center text-3xl font-bold cursor-pointer gap-4 text-[#a50034] hover:opacity-80 transition">
+            <svg className="w-12 h-12 fill-[#a50034] flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+              <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/>
+            </svg> History
+          </button>
+
+          <button className="flex items-center text-3xl font-bold cursor-pointer gap-4 text-[#a50034] hover:opacity-80 transition">
+            <svg className="w-12 h-12 fill-[#a50034] flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+              <path d="M320 64C302.3 64 288 78.3 288 96L288 99.2C215 114 160 178.6 160 256L160 277.7C160 325.8 143.6 372.5 113.6 410.1L103.8 422.3C98.7 428.6 96 436.4 96 444.5C96 464.1 111.9 480 131.5 480L508.4 480C528 480 543.9 464.1 543.9 444.5C543.9 436.4 541.2 428.6 536.1 422.3L526.3 410.1C496.4 372.5 480 325.8 480 277.7L480 256C480 178.6 425 114 352 99.2L352 96C352 78.3 337.7 64 320 64zM258 528C265.1 555.6 290.2 576 320 576C349.8 576 374.9 555.6 382 528L258 528z"/>
+            </svg> Notification
+          </button>
+        </div>
+      </div>
+
+      {/*Ini masih hardcode, nanti di ganti biar setting di ujung*/}
+      <div className="pt-92">
+        <button className="flex items-center gap-4 text-3xl font-bold cursor-pointer text-[#a50034] hover:opacity-80 transition">
+          <svg className="w-12 h-12 fill-[#a50034] flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+            <path d="M259.1 73.5C262.1 58.7 275.2 48 290.4 48L350.2 48C365.4 48 378.5 58.7 381.5 73.5L396 143.5C410.1 149.5 423.3 157.2 435.3 166.3L503.1 143.8C517.5 139 533.3 145 540.9 158.2L570.8 210C578.4 223.2 575.7 239.8 564.3 249.9L511 297.3C511.9 304.7 512.3 312.3 512.3 320C512.3 327.7 511.8 335.3 511 342.7L564.4 390.2C575.8 400.3 578.4 417 570.9 430.1L541 481.9C533.4 495 517.6 501.1 503.2 496.3L435.4 473.8C423.3 482.9 410.1 490.5 396.1 496.6L381.7 566.5C378.6 581.4 365.5 592 350.4 592L290.6 592C275.4 592 262.3 581.3 259.3 566.5L244.9 496.6C230.8 490.6 217.7 482.9 205.6 473.8L137.5 496.3C123.1 501.1 107.3 495.1 99.7 481.9L69.8 430.1C62.2 416.9 64.9 400.3 76.3 390.2L129.7 342.7C128.8 335.3 128.4 327.7 128.4 320C128.4 312.3 128.9 304.7 129.7 297.3L76.3 249.8C64.9 239.7 62.3 223 69.8 209.9L99.7 158.1C107.3 144.9 123.1 138.9 137.5 143.7L205.3 166.2C217.4 157.1 230.6 149.5 244.6 143.4L259.1 73.5zM320.3 400C364.5 399.8 400.2 363.9 400 319.7C399.8 275.5 363.9 239.8 319.7 240C275.5 240.2 239.8 276.1 240 320.3C240.2 364.5 276.1 400.2 320.3 400z"/>
+          </svg> Setting
+        </button>
+      </div>  
+    </aside>
+  );  
+}
