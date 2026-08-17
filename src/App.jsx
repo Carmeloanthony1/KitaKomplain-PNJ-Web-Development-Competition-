bb3a0b5927248767690dac190d095ed86bb42fe3
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-17 06:50:33 -0400

    add: menambahkan most polling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
 import Sidebar_Kiri from "./components/Sidebar_Kiri";
 import Navbar from "./components/Navbar";
+import Most_Polling from "./components/Most_Polling";
 
 export default function App() {
   return (
-    <div className="bg-[#f7f7f7] min-h-screen px-8 py-4">
-      <div className="flex gap-8 justify-between items-start">
-        <Sidebar_Kiri />
-        <div className="flex-1 flex flex-col gap-6">
-          <Navbar />
-          <div className="flex gap-8 justify-between items-start pt-4">
-          </div>
-        </div>
+    <div className="min-h-screen bg-[#f7f7f7] flex">
+      <Sidebar_Kiri />
+
+      <div className="flex-1 flex flex-col items-center px-6">
+        <Navbar />
+        <main className="w-full max-w-2xl mt-20">
+        </main>
+      </div>
+
+      <div className="p-6">
+        <Most_Polling />
       </div>
     </div>
   );
