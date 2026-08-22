44b849e65cb6c97b1b43b839cc669b70511225df
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-22 05:41:13 -0400

    add: handle submit dan little fix at variable name

--- src/components/newpost.jsx
+++ src/components/newpost.jsx
@@ -41,26 +41,87 @@ export function NewPost() {
         }
 
         setLoading(true);
-    }
+
+        try {
+            let imageURL = null;
+            if(image){
+                const fileEXT = image.name.split(".").pop();
+                const filename = `${userId}_${Date.now()}.${fileEXT}`;
+                const filepath = `posts/${filename}`;
+            
+
+                const { error: uploadError } = await supabase.storage
+                    .from("posts")
+                    .upload(filepath, image);
+                
+                if(uploadError) throw uploadError;
+
+                const { data: url_data } = supabase.storage
+                    .from("posts")
+                    .getPublicUrl(filepath);
+
+                imageURL = url_data.publicUrl;
+            }
+
+            const { error: insert_error } = await supabase.from("posts").insert([
+                {
+                    user_id: userId,
+                    title:title,
+                    description: description,
+                    tag: tag,
+                    image_url: imageURL
+                },
+            ]);
+
+            if(insert_error) throw insert_error;
+            alert("Postingan berhasil di buat");
+
+            setDescription("");
+            setTag("");
+            setImage(null);
+            setPreview(null);
+
+            navigate("/home");
+        } catch (error) {
+            console.error("Gagal membuat post:", error.message);
+            alert("Gagal mengirim postingan: " + error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return ( 
         <div className="flex ">
             <div className="flex flex-col flex-1">
-                <div className="mt-4 w-full bg-white rounded-lg shadow p-6">
+                <form onSubmit={handleSubmit} className="mt-4 w-full bg-white rounded-lg shadow p-6">
                     <h2 className="text-xl font-bold mb-4">Tuliskan pengalaman anda</h2>
-                    <textarea 
-                        placeholder="Apa yang ingin kamu bagikan?"
-                        className="w-full h-32 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
+                    
+                    {/* Input title */}
+                    <input  
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Judul postingan"
+                        className="w-full mt-3 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 font-semibold"
+                        >
+                    </input>
 
-                    {/* Input Lokasi */}
+                    {/* Input tag */}
                     <input
                         type="text"
-                        value={lokasi}
-                        onChange={(e) => setLokasi(e.target.value)}
-                        placeholder="Tambahkan lokasi..."
+                        value={tag}
+                        onChange={(e) => setTag(e.target.value)}
+                        placeholder="Tambahkan tag..."
                         className="w-full mt-3 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
 
+                    {/* Input Description */}
+                    <textarea 
+                        placeholder="Apa yang ingin kamu bagikan?"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="w-full h-32 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
                     {/* Input Gambar */}
                     <div className="mt-3">
                         <label className="flex items-center gap-2 cursor-pointer text-blue-500 hover:text-blue-600 w-fit">
@@ -94,10 +155,12 @@ export function NewPost() {
                         </div>
                     )}
 
-                    <button className="mt-4 bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-800 transition-colors duration-150">
-                        Posting
+                    <button type = "submit"
+                        disabled={loading} 
+                        className="mt-4 bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-800 transition-colors duration-150">
+                        {loading ? "Mengirim..." : "Posting"}                    
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     )
