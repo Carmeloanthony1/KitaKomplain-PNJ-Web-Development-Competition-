40012e9653578b18ea1102bdb741d7e633868ef7
marvelvg <marvelvg365@gmail.com>
2026-08-31 20:54:25 +0700

    test lg

--- src/components/newpost.jsx
+++ src/components/newpost.jsx
@@ -169,7 +169,7 @@ export function NewPost({ isOpen, onClose, onPostCreated }) {
             </div>
           )}
 
-{/* TEST PUSH 2*/}
+{/* TEST PUSH 23333*/}
           <div className="flex justify-end mt-5">
             <button
               type="submit"
